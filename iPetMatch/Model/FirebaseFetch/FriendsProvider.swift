3ca64706e0fe6604e5cb145bd3b35f133c87efdc
HsiaoAi <ilct227@gmail.com>
2017-12-30 23:08:43 +0800

    Rebuild match page

---
+++ iPetMatch/Model/FirebaseFetch/FriendsProvider.swift
@@ -0,0 +1,14 @@
+//
+//  FriendsProvider.swift
+//  iPetMatch
+//
+//  Created by Hsiao Ai LEE on 30/12/2017.
+//  Copyright © 2017 Hsiao Ai LEE. All rights reserved.
+//
+
+
+
+class FriendProvider {
+    
+    
+}
